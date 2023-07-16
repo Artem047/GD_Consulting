9cb82f881854ce8d5158d978411ca6683700e04b
Artem <artem.glebov2004@yandex.ru>
2023-07-16 11:53:12 +0300

    change marker on circle

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,22 +1,31 @@
 import React from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  Circle,
+  CircleMarker,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const Maps = () => {
   const position = [25.186348, 55.260535];
+  const fillBlueOptions = { fillColor: "blue" };
+
   return (
     <MapContainer
       center={position}
       zoom={13}
       scrollWheelZoom={false}
-      className="w-full h-[575px] rounded-[160px] my-24">
+      className="w-full h-[575px] my-24 rounded-tr-[160px] rounded-br-[160px]">
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position}>
+      <CircleMarker center={position} pathOptions={fillBlueOptions} radius={30}>
         <Popup>
           19th floor The Exchange Tower, Business <br /> Bay, Dubai, United Arab
           Emirates
         </Popup>
-      </Marker>
+      </CircleMarker>
     </MapContainer>
   );
 };
